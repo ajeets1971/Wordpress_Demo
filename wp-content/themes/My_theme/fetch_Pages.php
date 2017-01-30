33d03ac5f573a7af30dbfdaccd99402320203e0d
admin\lcom81_two <lanetteam.dipti>
2017-01-30 17:42:12 +0530

    Display contact Form in Front End and Numeric Pagination for Dispalying Post

--- wp-content/themes/My_theme/fetch_Pages.php
+++
@@ -1,7 +0,0 @@
- <?php /* Template Name: Pages - My Theme */ 
-
-    $pages = get_pages(); 
-    foreach ( $pages as $page ) {
-        echo '<li><a href="'. get_page_link( $page->ID ) .'">'.$page->post_title.'</a></li>';
-    }
-?>
--- wp-content/themes/My_theme/sidebar.php
+++
@@ -1,5 +0,0 @@
-<?php if ( is_active_sidebar( 'header_sidebar' ) ) : ?>
-	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-		<?php dynamic_sidebar( 'header_sidebar' ); ?>
-	</div><!-- #primary-sidebar -->
-<?php endif; ?>
